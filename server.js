297806d28f272894f1912f4aedbae2d608e49f23
Daniel Fernandez <dfernandezcode@gmail.com>
2023-09-03 15:34:16 +0200

    render.yaml file added for deploy to render

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');  // Importing path for file serving
+
+let fetch;
+import('node-fetch').then(module => {
+  fetch = module.default;
+});
+
+const app = express();
+
+require('dotenv').config();
+
+const apiKey = process.env.WEATHER_API_KEY;
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+// Serve static files from the frontend folder
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.use(cors());
+
+app.get('/weather/:city', async (req, res) => {
+  try {
+    if (!fetch) {
+      return res.status(500).json({ error: 'Server is initializing. Please try again later.' });
+    }
+    const city = req.params.city;
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Serve frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
